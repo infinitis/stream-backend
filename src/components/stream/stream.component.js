15775c75098148626a43020ba8a6cd7a3b1ded0b
alex <alex@infiniteadaptability.org>
2020-04-08 22:07:56 -0700

    Squashed 'client/' changes from c821e22..0f75900
    0f75900 ...
    
    git-subtree-dir: client
    git-subtree-split: 0f75900bc0a26ff11e6f4553aa4d5a74a41b7f12

--- src/components/stream/stream.component.js
+++ src/components/stream/stream.component.js
@@ -9,7 +9,7 @@ export default class Stream extends React.Component {
 	}
 	componentDidMount() {
 		console.log(hls);
-		console.log(new Hls());
+		console.log(new hls());
 	}
 	render() {
 		return createElement("div",{className:style.container},
