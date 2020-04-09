857a6d6fcd3b2963aece2e7bda7aaec75b9328b9
alex <alex@infiniteadaptability.org>
2020-04-08 21:35:32 -0700

    Squashed 'client/' changes from d4009b2..5677ecc
    5677ecc ...
    
    git-subtree-dir: client
    git-subtree-split: 5677ecc38ac73f77041aabf67752857d347db64e

--- src/components/main/main.component.js
+++ src/components/main/main.component.js
@@ -1,9 +1,12 @@
 import style from './main.css';
 
+import {stream} from '../stream/stream.js';
+
 export default class Main extends React.Component {
 	render() {
 		return createElement("div",null,
-			createElement("span",null,"Test SPAN")
+			createElement("span",null,"Test SPAN"),
+			createElement(stream,null)
 		);
 	}
 }
