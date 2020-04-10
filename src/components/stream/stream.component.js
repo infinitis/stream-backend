071add0c72fe47c1e4b32e3974b5ddd8dd0ae443
alex <alex@infiniteadaptability.org>
2020-04-10 01:33:24 -0700

    Squashed 'client/' changes from 929fca5..4c47f03
    4c47f03 ...
    
    git-subtree-dir: client
    git-subtree-split: 4c47f037df7660e5c52c505a22d67b1f4629a03e

--- src/components/stream/stream.component.js
+++ src/components/stream/stream.component.js
@@ -6,6 +6,8 @@ export default class Stream extends React.Component {
 	constructor(props) {
 		super(props);
 		this.videoRef = React.createRef();
+		this.manifest_found = this.manifest_found.bind(this);
+		this.handle_hls_error = this.handle_hls_error.bind(this);
 	}
 	componentDidMount() {
 		const player = this.videoRef;
@@ -13,13 +15,9 @@ export default class Stream extends React.Component {
 			const Hls = new hls();
 			Hls.loadSource(process.env.__STREAM_URL__);
 			Hls.attachMedia(player);
-			Hls.on(hls.Events.MANIFEST_PARSED,() => {
-				player.play();
-			});
-			Hls.on(hls.Events.ERROR,(event,data) => {
-				console.log(event);
-				console.log(data);
-			});
+			Hls.on(hls.Events.MANIFEST_PARSED,manifest_found);
+			Hls.on(hls.Events.ERROR,handle_hls_error);
+			this.hls_instance = Hls;
 		} else if(player.canPlayType('application/vnd.apple.mpegurl')) {
 			player.src = process.env.__STREAM_URL__;
 			player.addEventListener('loadmetadata', () => {
@@ -27,6 +25,30 @@ export default class Stream extends React.Component {
 			});
 		} else {
 			throw new Error("hls not supported");
+		}
+	}
+	manifest_found() {
+		console.log('play');
+		console.log(this.videoRef);
+		this.videoRef.play();
+	}
+	handle_hls_error(event,data) {
+		if (data.fatal) {
+			switch(data.type) {
+				case hls.ErrorTypes.NETWORK_ERROR:
+					// try to recover network error
+					console.log("fatal network error encountered, try to recover");
+					this.hls_instance.startLoad();
+					break;
+				case hls.ErrorTypes.MEDIA_ERROR:
+					console.log("fatal media error encountered, try to recover");
+					this.hls_instance.recoverMediaError();
+					break;
+				default:
+					// cannot recover
+					this.hls_instance.destroy();
+					break;
+			}
 		}
 	}
 	render() {
