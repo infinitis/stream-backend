8c0c7a4d936fb5314b5807aeead240c496b7158b
alex <alex@infiniteadaptability.org>
2020-04-10 01:15:07 -0700

    Squashed 'client/' changes from 76aa397..929fca5
    929fca5 ...
    
    git-subtree-dir: client
    git-subtree-split: 929fca5c6ae80929c33452b59549a5b7cc9619f1

--- src/components/stream/stream.component.js
+++ src/components/stream/stream.component.js
@@ -8,19 +8,22 @@ export default class Stream extends React.Component {
 		this.videoRef = React.createRef();
 	}
 	componentDidMount() {
+		const player = this.videoRef;
 		if(hls.isSupported()) {
 			const Hls = new hls();
 			Hls.loadSource(process.env.__STREAM_URL__);
-			Hls.attachMedia(this.videoRef);
-			console.log('here1');
+			Hls.attachMedia(player);
 			Hls.on(hls.Events.MANIFEST_PARSED,() => {
-				console.log('here');
-				this.videoRef.play();
+				player.play();
 			});
-		} else if(this.videoRef.canPlayType('application/vnd.apple.mpegurl')) {
-			this.videoRef.src = process.env.__STREAM_URL__;
-			this.videoRef.addEventListener('loadmetadata', () => {
-				this.videoRef.play();
+			Hls.on(hls.Events.ERROR,(event,data) => {
+				console.log(event);
+				console.log(data);
+			});
+		} else if(player.canPlayType('application/vnd.apple.mpegurl')) {
+			player.src = process.env.__STREAM_URL__;
+			player.addEventListener('loadmetadata', () => {
+				player.play();
 			});
 		} else {
 			throw new Error("hls not supported");
