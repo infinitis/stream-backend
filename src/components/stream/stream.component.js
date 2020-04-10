392b34dab570e07df447bb805c348b8f9d1ccc67
alex <alex@infiniteadaptability.org>
2020-04-10 02:24:52 -0700

    Squashed 'client/' changes from f5aae40..52064c6
    52064c6 ...
    
    git-subtree-dir: client
    git-subtree-split: 52064c666c83f0e7a22510dda598fc08949e22e3

--- src/components/stream/stream.component.js
+++ src/components/stream/stream.component.js
@@ -1,4 +1,4 @@
-import hls from 'hls.js';
+import {MediaPlayer} from 'dashjs';
 
 import style from './stream.css';
 
@@ -6,55 +6,15 @@ export default class Stream extends React.Component {
 	constructor(props) {
 		super(props);
 		this.videoRef = React.createRef();
-		this.manifest_found = this.manifest_found.bind(this);
-		this.handle_hls_error = this.handle_hls_error.bind(this);
 	}
 	componentDidMount() {
-		const player = this.videoRef;
-		if(hls.isSupported()) {
-			const Hls = new hls();
-			Hls.loadSource(process.env.__STREAM_URL__);
-			Hls.attachMedia(player);
-			Hls.on(hls.Events.MANIFEST_PARSED,this.manifest_found);
-			Hls.on(hls.Events.ERROR,this.handle_hls_error);
-			this.hls_instance = Hls;
-		} else if(player.canPlayType('application/vnd.apple.mpegurl')) {
-			player.src = process.env.__STREAM_URL__;
-			player.addEventListener('loadmetadata', () => {
-				player.play();
-			});
-		} else {
-			throw new Error("hls not supported");
-		}
-	}
-	manifest_found() {
-		console.log('play');
-		console.log(this.videoRef);
-		debugger;
-		this.videoRef.play();
-	}
-	handle_hls_error(event,data) {
-		if (data.fatal) {
-			switch(data.type) {
-				case hls.ErrorTypes.NETWORK_ERROR:
-					// try to recover network error
-					console.log("fatal network error encountered, try to recover");
-					this.hls_instance.startLoad();
-					break;
-				case hls.ErrorTypes.MEDIA_ERROR:
-					console.log("fatal media error encountered, try to recover");
-					this.hls_instance.recoverMediaError();
-					break;
-				default:
-					// cannot recover
-					this.hls_instance.destroy();
-					break;
-			}
-		}
+		const url = process.env.__STREAM_URL__;
+		const player = MediaPlayer().create();
+		player.initialize(this.videoRef,url,true)
 	}
 	render() {
 		return createElement("div",{className:style.container},
-			createElement("video",{ref:this.videoRef})
+			createElement("video",{ref:this.videoRef,className:style.player,controls:true})
 		);
 	}
 }
