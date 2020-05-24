a7e72127b53bd31d6c577c8db07b2ee405e71a1a
alex <alex@infiniteadaptability.org>
2020-05-23 20:53:06 -0700

    Squashed 'pull_relay/' changes from a5e82a3..19a87d8
    19a87d8 ...
    
    git-subtree-dir: pull_relay
    git-subtree-split: 19a87d809402ad2d65e2acd4633a826ef5e31625

---
+++ entrypoint.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cat > /etc/nginx/nginx.conf << EOF
+include /etc/nginx/modules-enabled/*.conf;
+
+events {
+	worker_connections 768;
+	# multi_accept on;
+}
+
+http {
+	server {
+		listen 80;
+		
+		location /dash {
+			types {
+				application/vnd.apple.mpegurl m3u8;
+			}
+			root /tmp;
+			add_header Cache-Control no-cache;
+
+			# To avoid issues with cross-domain HTTP requests (e.g. during development)
+            add_header Access-Control-Allow-Origin *;
+        }
+
+    	root /var/www/;
+        
+        location / {
+    		index	index.html;
+        }
+
+        location ~ \.js {
+		    add_header  Content-Type    application/x-javascript;
+		}
+    }
+}
+
+rtmp {
+	server {
+		listen 1935;
+		
+		application src {
+			live on;
+			pull $PULL_ENDPOINT?key=$ENDPOINT_KEY name=source static live=1;
+
+			dash on;
+			dash_path /tmp/dash;
+			dash_nested on;
+		}
+	}
+}
+EOF
+
+cat /etc/nginx/nginx.conf
+
+nginx -g 'daemon off;'
